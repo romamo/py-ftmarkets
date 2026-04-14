b8cb8aebf5f4ce7a2443932ee5939341c8b7e7c8
Romamo <romamo@yandex.ru>
2026-04-14 18:07:38 +0300

    refactor: use Security instead of Symbol in __init__.py

--- src/ftmarkets/__init__.py
+++ src/ftmarkets/__init__.py
@@ -1,7 +1,7 @@
-from pydantic_market_data.models import OHLCV, History, Symbol
+from pydantic_market_data.models import OHLCV, History, Security
 
 from .api import FTDataSource as FTDataSource
 
-__all__ = ["OHLCV", "History", "Symbol", "FTDataSource"]
+__all__ = ["OHLCV", "History", "Security", "FTDataSource"]
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
